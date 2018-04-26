34c96d28e4027b0b37be320bb8551aa3ff903f87
jmorgan20 <37450743+jmorgan20@users.noreply.github.com>
2018-04-26 13:30:48 -0500

    completed

--- src/P9_ATMDispenser.java
+++ src/P9_ATMDispenser.java
@@ -1,3 +1,4 @@
+
 /*
 	ISYS 320
 	Name(s):
@@ -10,17 +11,12 @@ public class P9_ATMDispenser {
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
 		System.out.println("How much money do you want to withdraw?");
-		
+
 		int money = console.nextInt();
-		
-		System.out.println("Dispensing " + money/10 + " ten dollar bills.");
-		System.out.println("Dispensing " + money%10 + " one dollar bills.");
-		
-		
-		
-		
-		
+
+		System.out.println("Dispensing " + money / 10 + " ten dollar bills.");
+		System.out.println("Dispensing " + money % 10 + " one dollar bills.");
+
 	}
 
 }
-
